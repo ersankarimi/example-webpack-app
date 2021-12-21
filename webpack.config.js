accb41e49ed4ad2f511bf0a2bd6602886527fbc4
Ersan Karimi <ersanb19@gmail.com>
2021-12-21 22:17:28 +0800

    Update webpack.config.js
    - Add config for HtmlWebpackPlugin
    - add [contenthash] in file name output

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
 	mode: "development",
 	entry: "./src/index.js",
 	output: {
-		filename: "app.js",
+		filename: "app.[contenthash].js",
 		path: path.resolve(__dirname, "dist"),
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Webpack App Demo",
+			template: "./src/template.html",
+		}),
+	],
 	module: {
 		rules: [
 			{
