e2db183a3a8ac55556d4e6b1c7326bfef422bb80
Ersan Karimi <ersanb19@gmail.com>
2021-12-21 15:47:13 +0800

    Import css file

--- src/app/component.service.js
+++ src/app/component.service.js
@@ -1,7 +1,11 @@
+import "./../input-value.css";
+
 export class ComponentService {
 	constructor() {
 		this.numberOneInput = document.getElementById("numberOne");
+		this.numberOneInput.classList.add("input-value");
 		this.numberTwoInput = document.getElementById("numberTwo");
+		this.numberTwoInput.classList.add("input-value");
 		this.addValuesButton = document.getElementById("addValues");
 		this.resultDiv = document.getElementById("result");
 	}
