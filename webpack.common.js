890f2cb9047b5b804e8e8c71c1f8ffcf87a87b02
Ersan Karimi <ersanb19@gmail.com>
2021-12-22 14:52:09 +0800

    Rename webpack.confog.js to webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
 
 module.exports = {
-	mode: "development",
 	entry: "./src/index.js",
-	output: {
-		filename: "app.[contenthash].js",
-		path: path.resolve(__dirname, "dist"),
-	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: "Webpack App Demo",
