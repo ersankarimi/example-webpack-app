2c108cbfc0be481eb0d043203325c92ac34de4a1
Ersan Karimi <ersanb19@gmail.com>
2021-12-23 15:46:49 +0800

    Update

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,26 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+	// entry
 	entry: {
 		app: "./src/index.js",
 		vendor: "./src/vendor.js",
 	},
+	output: {
+		clean: true,
+	},
+
+	// plugins
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: "Webpack App Demo",
 			template: "./src/template.html",
 		}),
 	],
+
+	// module
 	module: {
 		rules: [
-			{
-				test: /\.scss$/i,
-				use: ["style-loader", "css-loader", "sass-loader"],
-			},
 			{
 				test: /\.html$/i,
 				use: ["html-loader"],
