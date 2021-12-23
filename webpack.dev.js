7ec82ed0ffb5aae62afea719f0012fabec193359
Ersan Karimi <ersanb19@gmail.com>
2021-12-23 15:47:00 +0800

    Update

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,8 +5,21 @@ const { merge } = require("webpack-merge");
 module.exports = merge(commonConfig, {
 	mode: "development",
 	devtool: "inline-source-map",
+
+	// output
 	output: {
-		filename: "[name].js",
 		path: path.resolve(__dirname, "dist"),
+		filename: "./assets/js/[name].js",
+		assetModuleFilename: "./assets/[name][ext]",
+	},
+
+	// module
+	module: {
+		rules: [
+			{
+				test: /\.scss$/i,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
 	},
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,47 @@
 const path = require("path");
 const commonConfig = require("./webpack.common.js");
 const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(commonConfig, {
 	mode: "production",
 	devtool: "source-map",
+
+	// output
 	output: {
-		filename: "[name].[contenthash].js",
 		path: path.resolve(__dirname, "dist"),
-		assetModuleFilename: "assets/[name].[hash][ext]",
-		clean: true,
+		filename: "./assets/js/[name].[contenthash].js",
+		assetModuleFilename: "./assets/[name].[hash][ext]",
+	},
+
+	// optimization
+	optimization: {
+		minimize: true,
+		minimizer: [new TerserPlugin()],
+	},
+
+	// plugins
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "./assets/css/[name].[contenthash].css",
+		}),
+	],
+
+	// module
+	module: {
+		rules: [
+			{
+				test: /\.scss$/i,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {},
+					},
+					"css-loader",
+					"sass-loader",
+				],
+			},
+		],
 	},
 });
